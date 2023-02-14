0c77083118fd06861b71a299a32e626bff9e99bf
Dmon <rla99tjr@aitrics.com>
2023-02-14 18:18:30 +0900

    Project Create Avatar Remove Dev console

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -4,13 +4,13 @@ import { ProjectService } from './project.service';
 @Controller('project')
 export class ProjectController {
 
-  constructor(private service: ProjectService){}
+  constructor(private service: ProjectService) { }
 
   @Get()
-  getProjectLists(){
-    try{
+  getProjectLists() {
+    try {
 
-    }catch(error){
+    } catch (error) {
       console.log(error);
       console.log('프로젝트 조회중 에러발생');
       return error;
@@ -18,15 +18,15 @@ export class ProjectController {
   }
 
   @Post()
-  async createProject(@Body() res: any){
+  async createProject(@Body() res: any) {
 
-    try{
+    try {
 
       // Parameter
       const name: string = res.projectName;
       const response = await this.service.createProject(name);
       return response;
-    }catch(error){
+    } catch (error) {
 
       console.log(error);
       console.log('프로젝트 생성중 에러발생');
@@ -35,19 +35,17 @@ export class ProjectController {
   }
 
   @Post('/avatar')
-  async createAvatarModel(@Body() res: any){
-    try{
+  async createAvatarModel(@Body() res: any) {
+    try {
 
       console.log("---------컨트롤러-----------");
 
       // Parameter
       const avatar: object = res;
       const response = await this.service.createAvatar(avatar);
-      console.log(response);
-
       return response;
-      
-    }catch(error){
+
+    } catch (error) {
 
       console.log(error);
       console.log('프로젝트 아바타 모델 생성중 에러발생');
