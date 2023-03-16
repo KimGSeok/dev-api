8c27fe530b083665a46e1cf215bb2da0d3581a6b
Dmon <rla99tjr@aitrics.com>
2023-03-16 18:47:51 +0900

    project Avatar Generate Temp Develop

--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -3,30 +3,30 @@ import { HttpService } from '@nestjs/axios';
 import { ConnectionService } from "src/connection/connection.service";
 import { getProjectListQuery, createProjectQuery } from './project.query';
 
-interface KeyValueProps{
-  [key:string]: string
+interface KeyValueProps {
+  [key: string]: string
 }
 
 @Injectable()
-export class ProjectService{
+export class ProjectService {
 
   constructor(
     private connection: ConnectionService,
     private readonly httpService: HttpService
-  ){}
+  ) { }
 
   /**
    * Description: 프로젝트 생성
    * Date: 2023.03.11
    * Author: Kim Gyeong Seok
    */
-  async getProjectList(userInfo: KeyValueProps){
-    try{
+  async getProjectList(userInfo: KeyValueProps) {
+    try {
 
       // Query
       const [response, field] = await this.connection.connectionPool.query(getProjectListQuery, [userInfo.id]);
       return response;
-    }catch(error){
+    } catch (error) {
       console.error(error);
       console.log('프로젝트 목록 조회중 로직 에러발생');
       return error;
@@ -38,13 +38,13 @@ export class ProjectService{
    * Date: 2023.03.11
    * Author: Kim Gyeong Seok
    */
-  async createProject(name: string, userInfo: KeyValueProps, accessToken: string){
-    try{
+  async createProject(name: string, userInfo: KeyValueProps, accessToken: string) {
+    try {
 
       // Query
       const [response, field] = await this.connection.connectionPool.query(createProjectQuery, [userInfo.id, name]);
       return response;
-    }catch(error){
+    } catch (error) {
       console.error(error);
       console.log('프로젝트 생성중 로직 에러발생');
       return error;
@@ -56,8 +56,8 @@ export class ProjectService{
    * Date: 2023.03.11
    * Author: Kim Gyeong Seok
    */
-  async createAvatar(avatarInfo: any){
-    try{
+  async createAvatar(avatarInfo: any) {
+    try {
 
       console.log(avatarInfo);
 
@@ -65,24 +65,29 @@ export class ProjectService{
       const { avatar, voice, scriptList } = avatarInfo;
       const contentType = avatar.name === '' ? 'audio' : 'video';
       const mlAvatarArray = [];
-      
+
       // Machine Learing Data Argument Object
       scriptList.forEach((el: any) => {
 
-        // Audio(Voice)
-        mlAvatarArray.push({
-          'script': el.text,
-          'speed': el.speed,
-          'pause_second': el.pauseSecond,
-          'audio_twin_version': voice.model ? voice.model : '01831c53-3a8b-7a50-bd97-v16ch5f8d45s'
-        })
-
         // Video(Avatar)
-        if(contentType === 'video'){
+        if (contentType === 'video') {
           mlAvatarArray.push({
+            'script': el.text,
+            'speed': el.speed,
+            'pause_second': el.pauseSecond,
+            'audio_twin_version': voice.model ? voice.model : '01831c53-3a8b-7a50-bd97-v16ch5f8d45s',
             'cut_start_time': 0,
             'cut_end_time': -1,
             'video_twin_version': avatar.model
+          })
+        }
+        // Audio(Voice)
+        else {
+          mlAvatarArray.push({
+            'script': el.text,
+            'speed': el.speed,
+            'pause_second': el.pauseSecond,
+            'audio_twin_version': voice.model ? voice.model : '01831c53-3a8b-7a50-bd97-v16ch5f8d45s'
           })
         }
       });
@@ -95,12 +100,18 @@ export class ProjectService{
         }
       };
 
+      console.log(FAST_API_URL);
+      console.log(mlAvatarArray);
+
       // TODO Response오면 DB Insert
       const response: any = await this.httpService.post(FAST_API_URL, mlAvatarArray, options).toPromise();
 
-      if(response.data.result === 'failed'){
+      if (response.data.result === 'failed') {
         return new ServiceUnavailableException();
       }
+
+      console.log(response);
+      console.log(response.data);
 
       const rawData = await fetch(`http://fury.aitricsdev.com:40068${response.data.audio_path}`);
       const blob = await rawData.blob();
@@ -110,7 +121,7 @@ export class ProjectService{
       result.arrayBuffer = Object.values(new Uint8Array(await blob.arrayBuffer()));
 
       return JSON.stringify(result);
-    }catch(error){
+    } catch (error) {
       console.error(error);
       console.log('프로젝트 아바타 모델 생성중 로직 에러발생');
       return error;
